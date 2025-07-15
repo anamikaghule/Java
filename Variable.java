96717a9f59f2fd87b45346a534df77cd2a9bd081
anamikaghule <ghuleanamika004@gmail.com>
2025-07-15 16:19:21 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,19 @@
+package pqr;
+
+public class Variable {
+	public static void main(String[]args) {
+		int a = 40;
+		int b = 30;
+		float avg = (a+b)/2.0f;
+		char initial = 'J';
+		boolean av = true;
+		String name = "anu";
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("average:"+avg);
+		System.out.println("initial:"+initial);
+		System.out.println("av:"+av);
+		System.out.println("name:"+name);
+	}
+
+}  
