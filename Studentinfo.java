b119e596246707f791b5a3d8e195da9b0e9ed84d
anamikaghule <ghuleanamika004@gmail.com>
2025-07-21 18:50:46 +0530

    Add files via upload

---
+++ Studentinfo.java
@@ -0,0 +1,14 @@
+package abc;
+public class Studentinfo {
+	public static void main(String[]args) {
+		Fybcs f1 = new Fybcs();
+		f1.Display();
+		Sybcs s1 = new Sybcs();
+		s1.Display();
+		Tybcs t1 = new Tybcs();
+		t1.Display();
+		
+		
+	}
+		
+}
