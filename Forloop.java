e4b7350574656380f2ef0dc09bdb640d52ec8996
anamikaghule <ghuleanamika004@gmail.com>
2025-08-02 15:12:36 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,20 @@
+package abc;
+
+public class Forloop {
+	public static void main(String[]args) {
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=i;j++)
+		{
+			System.out.printf("* ");	
+
+		}
+				System.out.println("   ");
+					
+
+			}
+		}
+		
+}
+	
+
